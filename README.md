f7ee95212ca7af7e74f88e83daeeed112fb073f9
Tony <yizhe.cheng@mail.utoronto.ca>
2019-04-28 01:25:21 -0400

    added a start file for the server.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Community-Market
+# Proximity
---
+++ server/start.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3333;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
