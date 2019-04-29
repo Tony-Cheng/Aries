2ffdd1b7aefdb32e6098c0df1d0dfbd490d90134
Tony <yizhe.cheng@mail.utoronto.ca>
2019-04-28 21:48:59 -0400

    added an expressjs example.

--- server/start.js
+++ server/start.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 
 const http = require('http');
+var express = require('express')
+var app = express()
 
 const hostname = '0.0.0.0';
 const port = 3333;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+app.get("/", (req, res) => {
+  res.send('Received a default request!\n')
+})
+app.get("/login", (req, res) => {
+  res.send('Received a login request!\n')
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => console.log(`Proximity app listening on port ${port}!`))
