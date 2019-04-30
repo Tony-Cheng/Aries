6d1086d68464e18657ee277840249fcc2046b451
Tony <yizhe.cheng@mail.utoronto.ca>
2019-04-30 02:02:03 -0400

    formatted the files

--- server/login/store.js
+++ server/login/store.js
@@ -1,42 +1,35 @@
-hash_SHA3 = function(pass) {
-    hash.update(pass);
-    return hash.digest(encoding='hex');
-}
-
-exports.storeUsername = function(user) {
-    con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected! About to store Username...");
-        var sql = "INSERT INTO login (username, password) VALUES (${user}, 'Highway 37')";
-        con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("1 record inserted");
-      });
-    });
-}
-
-exports.storePassword = function(pass) {
-    var hashedPassword = hash_SHA3(pass);
-    
-}
-
 const { SHA3 } = require('sha3');
 const hash = new SHA3(256);
 
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-    host: "http://mysql.tcheng.ca",
-    user: "server_test",
-    password: "Testserver123!",
-    database: "Proximity"
-});
+function hash_SHA3(pass) {
+    hash.update(pass);
+    return hash.digest(encoding = 'hex');
+}
+
+exports.storeUsername = function (user) {
+    var con = mysql.createConnection({
+        host: "mysql.tcheng.ca",
+        user: "server_test",
+        password: "Testserver123!",
+        database: "Proximity"
+    });
+
+    con.connect(function (err) {
+        if (err) throw err;
+        user = '';
+        console.log("Connected! About to store Username...");
+        var sql = 'INSERT INTO login (username, password) VALUES (${user}, \'Highway 37\')';
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("1 record inserted");
+        });
+    });
+}
+
+exports.storePassword = function (pass) {
+    var hashedPassword = hash_SHA3(pass);
+}
 
 exports.storeUsername("test");
-
-
-
-
-
-
-
--- server/start.js
+++ server/start.js
@@ -8,7 +8,14 @@ const port = 3333;
 app.get("/", (req, res) => {
   res.send('Received a default request!\n')
 })
+
+// server.tcheng.ca:3333/login
 app.get("/login", (req, res) => {
+  console.log('Proximity app listening on port ${req.username}!')
+})
+
+app.get("/register", (req, res) => {
+  
 })
 
 app.listen(port, () => console.log(`Proximity app listening on port ${port}!`))
