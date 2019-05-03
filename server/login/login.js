1b6628fba21cabb788066f4e7de78f1319c3886a
Michael-Chunman-Lee <michaelchunman.lee@mail.utoronto.ca>
2019-05-03 01:50:23 -0400

    Completed login.js

---
+++ server/login/login.js
@@ -0,0 +1,31 @@
+var loginIO = require("./loginIO");
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var session = require("express-session");
+
+var app = express();
+var IO = new loginIO();
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.get('/', function(request, response){
+    response.sendFile(path.join(__dirname + '/login.html'))
+});
+
+app.post('/auth', function(request, response) {
+    var username = request.body.username;
+    var password = request.body.password;
+    if (username && password) { 
+        IO.check(username, password).then().catch((err) => {
+        response.send(err);
+        response.end();
+        });
+    }
+})
+
+app.listen(3000);
