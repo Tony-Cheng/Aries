93b173eb397d7e22cd9bbc2b07bb932d4b688c2d
Michael-Chunman-Lee <michaelchunman.lee@mail.utoronto.ca>
2019-05-02 22:28:14 -0400

    Added error catching functionality

--- server/login/register.js
+++ server/login/register.js
@@ -1,11 +1,11 @@
-var store = require("./store.js");
+var loginIO = require("./loginIO");
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 var session = require("express-session");
 
 var app = express();
-
+var IO = new loginIO();
 app.use(session({
     secret: 'secret',
     resave: true,
@@ -21,7 +21,11 @@ app.post('/auth', function(request, response) {
     var username = request.body.username;
     var password = request.body.password;
     if (username && password) { 
-        store.register(username, password).catch(function(err){}).then(function(res){});
+        IO.store(username, password).then().catch(() => {
+        response.send("Entered an existing username!");
+        response.end();
+        });
     }
 })
+
 app.listen(3000);
