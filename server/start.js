9f7e7833fe8ca620dc8aaa93199bdfb42a3e1b51
Tony <yizhe.cheng@mail.utoronto.ca>
2019-05-10 20:11:44 -0400

    addded the login and register functionality to start.js.

--- server/start.js
+++ server/start.js
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
 
-var express = require('express')
-var app = express()
+var express = require('express');
+var session = require("express-session");
+var bodyParser = require("body-parser")
+var loginSystem = require("./login/login");
+var app = express();
 
 const port = 3333;
+
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send('Received a default request!\n')
@@ -11,11 +23,11 @@ app.get("/", (req, res) => {
 
 // server.tcheng.ca:3333/login
 app.get("/login", (req, res) => {
-  console.log('Proximity app listening on port ${req.username}!')
+  loginSystem.login(req, res);
 })
 
 app.get("/register", (req, res) => {
-  
+  loginSystem.register(req, res);
 })
 
 app.listen(port, () => console.log(`Proximity app listening on port ${port}!`))
