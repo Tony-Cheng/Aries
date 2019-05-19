fe14e8fecec43a215e83ba7015c23cf9c172e64e
Michael-Chunman-Lee <michaelchunman.lee@mail.utoronto.ca>
2019-05-19 14:00:35 -0400

    Template for HandleMessage Class

--- server/HandleMessage.js
+++ server/HandleMessage.js
@@ -17,11 +17,12 @@ module.exports = class {
     /**
      * Store the message in the database
      * @param {String} message 
-     * @param {String} username
+     * @param {String} to
+     * @param {String} from
      * @param {INT} chatid
      * @param {INT} timestamp
      */
-    storeMessage(username, message, chatid, timestamp) {
+    storeMessage(to, from, message, chatid, timestamp) {
         return new Promise((resolve, reject) => {
             let con = this.con;
             con.connect(function (err) {
@@ -30,15 +31,44 @@ module.exports = class {
                     reject("Connection Failed");
                 }
             });
-            let sql = `INSERT INTO chat (username, chatid, timestamp, message) VALUES \('${username}', '${message}', '${chatid}', '${timestamp}')`;
+            let sql = `INSERT INTO chat (username, chatid, timestamp, message) VALUES \('${to}', '${from}', '${chatid}', '${timestamp}', '${message}')`;
             con.query(sql, (err, res, field) => {
                 con.end();
                 if (err) {
-                    
+                    console.log(err);
+                    reject("Query Failed");
+                } else {
+                    console.log("Successfully inserted message");
+                    resolve("Success");
                 }
             })
         })
     }
 
+    /**
+     * Retrieve all the messages of an instance of a chat between two users
+     * @param {INT} chatid
+     */
+    retrieveMessages(chatid) {
+        //TODO figure out how to return an array for promise 
+        return new Promise((resolve, reject) => {
+            let con = this.con;
+            con.connect(function (err) {
+                if (err) {
+                    console.log(err)
+                }
+                //TODO: complete the body below
+            })
+        });
+    }
+
+    /**
+     * Retrieve all the appropriate chatids associate with a user
+     * @param {STRING} username
+     * @param {INT} chatid
+     */
+    getAllConversations(username, chatid) {
+        //TODO same as above
+    }
 }
  
