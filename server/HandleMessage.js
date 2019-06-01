7adfa6ecec0e0429ee37975d21676dc880e4f06e
Michael-Chunman-Lee <michaelchunman.lee@mail.utoronto.ca>
2019-06-01 14:23:51 -0400

    Added new sql query for adding and retrieving chatids

--- server/HandleMessage.js
+++ server/HandleMessage.js
@@ -73,10 +73,10 @@ module.exports = class {
 
     /**
      * Retrieve all the appropriate chatids associate with a user
-     * @param {STRING} username
-     * @param {INT} chatid
+     * @param {STRING} username1
+     * @param {STRING} username2
      */
-    getAllConversations(username, chatid) {
+    getAllConversations(username1, username2) {
         return new Promise((resolve, reject) => {
             let con = this.con;
             con.connect(function (err) {
@@ -84,7 +84,7 @@ module.exports = class {
                     console.log(err)
                 }
             });
-            let sql = `SELECT * FROM chat WHERE chatid='${chatid} AND username1='${username}' OR username2='${username}'`
+            let sql = `SELECT * FROM chat WHERE username1='${username1}' OR username2='${username2}'`
             con.query(sql, (err, res, field) => {
                 con.end();
                 if (err) {
@@ -93,6 +93,34 @@ module.exports = class {
                 } else {
                     console.log("Successfully retrieved chatids");
                     resolve(res);
+                }
+            })
+        })
+    }
+
+    /**
+     * Store new chatid of a conversation between two users.
+     * @param {STRING} username1
+     * @param {STRING} username2
+     * @param {INT} chatid
+     */
+    storeNewConversation(username1, username2, chatid) {
+        return new Promise((resolve, reject) => {
+            let con = this.con;
+            con.connect(function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            let sql = `INSERT INTO chatids (username1, username2, chatid) VALUES \('${username1}', '${username2}', '${chatid}')`;
+            con.query(sql, (err, res, field) => {
+                con.end();
+                if (err) {
+                    console.log(err);
+                    reject("Query Failed");
+                } else {
+                    console.log("Successfully inserted new conversation");
+                    resolve("Success");
                 }
             })
         })
