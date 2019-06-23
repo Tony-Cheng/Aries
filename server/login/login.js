9c8036e7359a7bf8bb24fb39c4b9637f9dab5976
Tony <yizhe.cheng@mail.utoronto.ca>
2019-06-23 00:28:06 -0400

    added login functionality.

--- server/login/login.js
+++ server/login/login.js
@@ -14,9 +14,9 @@ module.exports = class {
         this.app.post("/aries/server/register", (req, res) => {
             register(req, res);
         });
-        // this.app.post("/aires/server/login", (req, res) => {
-        //   login(req, res);
-        // });
+        this.app.post("/aries/server/login", (req, res) => {
+            login(req, res);
+        });
     }
 
 }
@@ -25,11 +25,18 @@ function login(request, response) {
     var IO = new loginIO();
     var username = request.body.username;
     var password = request.body.password;
+    response.setHeader('content-type', 'application/json');
     if (username && password) {
-        IO.check(username, password).then().catch((err) => {
-            response.send(err);
-            response.end();
+        IO.check(username, password).then(() => {
+            request.session.loggedIn = true;
+            request.session.username = username;
+            response.end(JSON.stringify({ status: "Success" }))
+        }).catch((err) => {
+            response.end(JSON.stringify({ status: "Incorrect username or password!" }));
         });
+    }
+    else {
+        response.end(JSON.stringify({ status: "Username and password cannot be empty!" }));
     }
 }
 
--- website/assets/js/login.js
+++ website/assets/js/login.js
@@ -3,10 +3,8 @@ function register() {
   var password = document.getElementById("inputpassword").value;
   var checkPassword = document.getElementById("checkpassword").value;
   if (password != checkPassword) {
-    return false;
-  }
-  else if (password == "" || username == "") {
-    return false;
+    $('#errorMessage').text("Password doesn't match!");
+    $('#errorDisplay').removeClass('d-none');
   }
   $.ajax({
     url: '/aries/server/register',
@@ -18,6 +16,31 @@ function register() {
     success: function (data, textStatus, jQxhr) {
       if (data.status == "Success") {
         window.location.href = 'login';
+      }
+      else {
+        $('#errorMessage').text(data.status);
+        $('#errorDisplay').removeClass('d-none');
+      }
+    },
+    error: function (jqXhr, textStatus, errorThrown) {
+      console.log('failed');
+    }
+  });
+}
+
+function login() {
+  var username = document.getElementById("inputusername").value;
+  var password = document.getElementById("inputpassword").value;
+  $.ajax({
+    url: '/aries/server/login',
+    dataType: 'json',
+    type: 'post',
+    contentType: 'application/json',
+    data: JSON.stringify({ "username": username, "password": password }),
+    processData: false,
+    success: function (data, textStatus, jQxhr) {
+      if (data.status == "Success") {
+        window.location.href = 'index';
       }
       else {
         $('#errorMessage').text(data.status);
