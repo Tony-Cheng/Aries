7ed841825eb19211259eb84c4fc50e5b0f8accab
Michael-Chunman-Lee <michaelchunman.lee@mail.utoronto.ca>
2019-07-25 19:28:26 -0400

    Added input fields

--- messenger/src/App.js
+++ messenger/src/App.js
@@ -3,6 +3,40 @@ import React from "react";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
+class Input extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {text: ""};
+  }
+
+  onChange(e) {
+    this.setState({text: e.target.value});
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    this.setState({text: ""});
+    this.props.onSendMessage(this.state.text);
+  }
+
+  render() {
+    return (
+      <div className="Input">
+        <form onSubmit={e => this.onSubmit(e)}>
+          <input
+            onChange={e => this.onChange(e)}
+            value={this.state.text}
+            type="text"
+            placeholder="Enter a message..."
+            autoFocus="true"
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
 class Messages extends React.Component {
 
   renderMessage(message) {
@@ -32,27 +66,49 @@ class Messages extends React.Component {
 
 
 class App extends React.Component { 
-  state = {
-    messages: [
-      {
-        text: "test",
-        user: {
-          colour: "#00FF00",
-          username: "user2"
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [
+        {
+          text: "test",
+          user: {
+            colour: "#00FF00",
+            username: "user2"
+          }
         }
+      ],
+      user: {
+        username: "user1",
+        colour: "#008000"
       }
-    ],
-    user: {
-      username: "user1",
-      colour: "#008000"
     }
   }
+
+   onSendMessage = (message) => {
+    const messages = this.state.messages;
+    messages.push({
+      text: message,
+      user: this.state.user
+    });
+    this.setState({messages: messages});
+  }
+
   render() {
     return (
       <div className="App">
+        <div className="App-header">
+          <h1>
+            Aries Messenger
+          </h1>
+        </div>
         <Messages 
           messages={this.state.messages}
           currentUser={this.state.user}/>
+        <Input
+          onSendMessage={this.onSendMessage}
+        />
       </div>
       /*
       <div className="App">
