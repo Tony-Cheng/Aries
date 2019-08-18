d944c74941ac3d8218a3f255e6ea649076c6b5be
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-08-17 21:42:52 -0400

    added the toxicity check api.

---
+++ server/utils/api.js
@@ -0,0 +1,25 @@
+exports.isToxic = (url, message) => {
+    return new Promise((resolve, reject) => {
+        var options = {
+            method: 'GET',
+            url: url,
+            headers:
+            {
+                'cache-control': 'no-cache',
+                Connection: 'keep-alive',
+                'Accept-Encoding': 'gzip, deflate',
+                'Cache-Control': 'no-cache',
+                Accept: '*/*',
+                'User-Agent': 'PostmanRuntime/7.15.2',
+                'Content-Type': 'text/plain'
+            },
+            'timeout': 10000,
+            body: message,
+        };
+
+        request(options, function (error, response, body) {
+            if (error) reject(error);
+            resolve(body == 'True\n');
+        });
+    });
+}
