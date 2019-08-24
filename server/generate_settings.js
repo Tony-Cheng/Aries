4abc5058d5d1db81c9b7fdb19025a7431c609cb8
Michael-Chunman-Lee <michaelchunman.lee@mail.utoronto.ca>
2019-08-23 22:26:59 -0400

    Fixed merge issue in generate_settings

--- server/generate_settings.js
+++ server/generate_settings.js
@@ -2,16 +2,10 @@ const fs = require('fs');
 
 settings = {
     "mongo": {
-<<<<<<< HEAD
-        "host": "aries.tcheng.ca",
-        "user": "memes888",
-        "password": "Memes888"
-=======
         "host": "",
         "user": "",
         "password": "",
         "database":"aries"
->>>>>>> aa0df9dc2916610ed358d8a946c4a74e45e6a83a
     },
     "mysql": {
         "host": "aries.tcheng.ca",
