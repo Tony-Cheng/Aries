e918a1e51e46314a3c810b67fb4b254b5d87b4b0
Michael-Chunman-Lee <michaelchunman.lee@mail.utoronto.ca>
2019-08-29 01:04:08 -0400

    Fixed formatting and some bugs

--- messenger/src/App.js
+++ messenger/src/App.js
@@ -11,7 +11,7 @@ import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem,
+  DropdownItem
 } from "reactstrap";
 import ClientSocket from "socket.io-client";
 import Cookies from "js-cookie";
@@ -85,43 +85,55 @@ class App extends React.Component {
     this.socket = new ClientSocket().connect();
     this.toggle = this.toggle.bind(this);
 
-    this.socket.on('newConnectedUser', (user) => {
-      this.socket.emit('addConnectedUser', user);
+    this.socket.on("newConnectedUser", user => {
+      this.socket.emit("addConnectedUser", user);
     });
-    
-    this.socket.on('receiveMessage', (msg) => {
+
+    this.socket.on("receiveMessage", msg => {
       const messages = this.state.messages;
-      if (msg.userid === this.state.curChatUser.userid) {
+      if (msg.userid === this.state.user.userid) {
         messages.push({
           text: msg.text,
           user: this.state.user
         });
-        this.setState({ messages: messages }); 
-        var element = document.getElementById('Messages-list');
-        element.scrollTop = element.scrollHeight - element.clientHeight;
+      } else {
+        messages.push({
+          text: msg.text,
+          user: {
+            colour: "#00FF00",
+            username: this.state.curChatUser.username
+          }
+        });
       }
+      this.setState({ messages: messages });
+      var element = document.getElementById("Messages-list");
+      element.scrollTop = element.scrollHeight - element.clientHeight;
     });
 
-    this.socket.on('initializeSearch', result => {
+    this.socket.on("initializeSearch", result => {
       var index;
       var newList = [];
       for (var i = 0; i < result.length; i++) {
         if (result[i].user_id === this.state.user.userid) {
           index = i;
         }
-        newList.push({value: result[i].user_id, label: result[i].username});
+        newList.push({ value: result[i].user_id, label: result[i].username });
       }
-      newList.splice(index, 1)
-      this.setState({userList: newList});
-      this.socket.emit('initializeChat', {userid: this.state.user.userid});
+      newList.splice(index, 1);
+      this.setState({ userList: newList });
+      this.socket.emit("initializeChat", { userid: this.state.user.userid });
     });
 
-    this.socket.on('retrieveFirstMessages', (res) => {
+    this.socket.on("retrieveFirstMessages", res => {
       var newFriendsList = [];
       for (var i = 0; i < res.IDs.length; i++) {
         for (var j = 0; j < this.state.userList.length; j++) {
           if (res.IDs[i] === this.state.userList[j].value) {
-            newFriendsList.push({username: this.state.userList[j].label, userid: this.state.userList[j].value, chatid: res.userChatIDs[i]});
+            newFriendsList.push({
+              username: this.state.userList[j].label,
+              userid: this.state.userList[j].value,
+              chatid: res.userChatIDs[i]
+            });
             break;
           }
         }
@@ -146,15 +158,20 @@ class App extends React.Component {
           });
         }
       }
-      this.setState({messages: newMessages});
-      this.setState({curChatUser: {username: newFriendsList[0].username, userid: newFriendsList[0].userid}});
-      this.setState({friendsList: newFriendsList});
-      this.setState({curChatID: newFriendsList[0].chatid});
-      var element = document.getElementById('Messages-list');
+      this.setState({ messages: newMessages });
+      this.setState({
+        curChatUser: {
+          username: newFriendsList[0].username,
+          userid: newFriendsList[0].userid
+        }
+      });
+      this.setState({ friendsList: newFriendsList });
+      this.setState({ curChatID: newFriendsList[0].chatid });
+      var element = document.getElementById("Messages-list");
       element.scrollTop = element.scrollHeight - element.clientHeight;
     });
 
-    this.socket.on('retrieveNewChat', (res) => {
+    this.socket.on("retrieveNewChat", res => {
       var newMessages = [];
       for (var k = 0; k < res.messages.length; k++) {
         if (res.messages[k].user_id === this.state.user.userid) {
@@ -175,23 +192,31 @@ class App extends React.Component {
           });
         }
       }
-      this.setState({messages: newMessages});
-      this.setState({curChatUser: {username: res.username, userid: res.userid}});
-      this.setState({curChatID: res.chatid});
-      var element = document.getElementById('Messages-list');
+      this.setState({ messages: newMessages });
+      this.setState({
+        curChatUser: { username: res.username, userid: res.userid }
+      });
+      this.setState({ curChatID: res.chatid });
+      var element = document.getElementById("Messages-list");
       element.scrollTop = element.scrollHeight - element.clientHeight;
     });
 
-    this.socket.on("AddedChat", (res) => {
+    this.socket.on("AddedChat", res => {
       if (!res.doesExist) {
         var updatedSelectedUser = this.state.selectedUser;
         var updatedFriendsList = this.state.friendsList;
-        updatedFriendsList.push({username: res.username, userid: res.userid, chatid: res.chatid});
+        updatedFriendsList.push({
+          username: res.username,
+          userid: res.userid,
+          chatid: res.chatid
+        });
         updatedSelectedUser.chatid = res.chatid;
-        this.setState({selectedUser: updatedSelectedUser});
-        this.setState({messages: []});
-        this.setState({curChatUser: {username: res.username, userid: res.userid}});
-        this.setState({curChatID: res.chatid});
+        this.setState({ selectedUser: updatedSelectedUser });
+        this.setState({ messages: [] });
+        this.setState({
+          curChatUser: { username: res.username, userid: res.userid }
+        });
+        this.setState({ curChatID: res.chatid });
       }
     });
 
@@ -206,46 +231,63 @@ class App extends React.Component {
         }
       ],
       user: {
-        username: Cookies.get('username'),
+        username: Cookies.get("username"),
         colour: "#008000",
-        userid: parseInt(Cookies.get('user_id'))
+        userid: parseInt(Cookies.get("user_id"))
       },
       dropdownOpen: false,
       friendsList: [],
       userList: [],
-      selectedUser: {username: "", userid: -1},
-      curChatUser: {username: "", userid: -1},
+      selectedUser: { username: "", userid: -1 },
+      curChatUser: { username: "", userid: -1 },
       curChatID: -1
     };
   }
 
-  onFriendClick = (event) => {
+  onFriendClick = event => {
     for (var i = 0; i < this.state.friendsList.length; i++) {
-      if (this.state.friendsList[i].username === event.currentTarget.textContent) {
+      if (
+        this.state.friendsList[i].username === event.currentTarget.textContent
+      ) {
         var userid = this.state.friendsList[i].userid;
         break;
       }
     }
-    this.socket.emit('changeChatUser', {chatUserID: userid, chatID: event.target.value, username: event.currentTarget.textContent});
-  }
+    this.socket.emit("changeChatUser", {
+      chatUserID: userid,
+      chatID: event.target.value,
+      username: event.currentTarget.textContent
+    });
+  };
 
   onSendMessage = message => {
-    this.socket.emit('newMessage', {userid: this.state.user.userid, text: message, chatid: this.state.curChatID, userid2: this.state.curChatUser.userid});
+    this.socket.emit("newMessage", {
+      userid: this.state.user.userid,
+      text: message,
+      chatid: this.state.curChatID,
+      userid2: this.state.curChatUser.userid
+    });
   };
 
   toggle() {
-    this.setState(prevState => ({ 
+    this.setState(prevState => ({
       dropdownOpen: !prevState.dropdownOpen
     }));
   }
 
   onUserListChange = event => {
-    this.setState({selectedUser: {username: event.label, userid: event.value}});
+    this.setState({
+      selectedUser: { username: event.label, userid: event.value }
+    });
     //Idea for group chat: have two prompts if user already in a chat have second confirm window to add to current chat (and have current chat as active tag under dropdownitem)
     if (window.confirm("Are you sure you would like to add this user?")) {
-      this.socket.emit("NewTwoPersonChat", {user2: event.value, user1: this.state.user.userid, username: event.label});
+      this.socket.emit("NewTwoPersonChat", {
+        user2: event.value,
+        user1: this.state.user.userid,
+        username: event.label
+      });
     }
-  }
+  };
 
   render() {
     if (this.state.friendsList.length === 0) {
@@ -261,13 +303,20 @@ class App extends React.Component {
                     Friends List
                   </DropdownToggle>
                   <DropdownMenu right>
-                    {this.state.friendsList.map((user) => <DropdownItem>{user.username}</DropdownItem>)}
+                    {this.state.friendsList.map(user => (
+                      <DropdownItem>{user.username}</DropdownItem>
+                    ))}
                   </DropdownMenu>
                 </UncontrolledDropdown>
               </Nav>
             </Collapse>
           </Navbar>
-          <Select value={this.state.selectedUser.username} placeholder="Find a friend..." onChange={this.onUserListChange} options={this.state.userList}/>
+          <Select
+            value={this.state.selectedUser.username}
+            placeholder="Find a friend..."
+            onChange={this.onUserListChange}
+            options={this.state.userList}
+          />
           <h1 className="No-Friends">
             Please add your first friend to begin chatting!
           </h1>
@@ -276,23 +325,35 @@ class App extends React.Component {
     } else {
       return (
         <div className="App">
-            <Navbar color="light" light expand="md">
-              <NavbarBrand color="black">Aries</NavbarBrand>
-              <NavbarToggler onClick={this.toggle} />
-              <Collapse isOpen={this.state.isOpen} navbar>
-                <Nav className="ml-auto" navbar>
-                  <UncontrolledDropdown nav inNavbar>
-                    <DropdownToggle nav caret>
-                      Friends List
-                    </DropdownToggle>
-                    <DropdownMenu right>
-                      {this.state.friendsList.map((user) => <DropdownItem value={user.chatid} onClick={this.onFriendClick}>{user.username}</DropdownItem>)}
-                    </DropdownMenu>
-                  </UncontrolledDropdown>
-                </Nav>
-              </Collapse>
-            </Navbar>
-            <Select value={this.state.selectedUser.username} placeholder="Find a friend..." onChange={this.onUserListChange} options={this.state.userList}/>
+          <Navbar color="light" light expand="md">
+            <NavbarBrand color="black">Aries</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    Friends List
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    {this.state.friendsList.map(user => (
+                      <DropdownItem
+                        value={user.chatid}
+                        onClick={this.onFriendClick}
+                      >
+                        {user.username}
+                      </DropdownItem>
+                    ))}
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </Collapse>
+          </Navbar>
+          <Select
+            value={this.state.selectedUser.username}
+            placeholder="Find a friend..."
+            onChange={this.onUserListChange}
+            options={this.state.userList}
+          />
           <h1 className="Conversation-friends">
             {this.state.curChatUser.username}
           </h1>
@@ -303,7 +364,7 @@ class App extends React.Component {
           <Input onSendMessage={this.onSendMessage} />
         </div>
       );
-    } 
+    }
   }
 }
 
--- server/server_listener.js
+++ server/server_listener.js
@@ -1,81 +1,115 @@
 module.exports = class {
+  constructor(io, messagingDB, loginSystem) {
+    this.io = io;
+    this.messagingDB = messagingDB;
+    this.loginSystem = loginSystem;
+  }
 
-    constructor(io, messagingDB, loginSystem) {
-        this.io = io;
-        this.messagingDB = messagingDB;
-        this.loginSystem = loginSystem;
-    }
+  initializeAllListeners() {
+    var userIDs = {};
+    var socketIDs = {};
 
-    initializeAllListeners() {
-        var userIDs = {};
-        var socketIDs = {};
+    this.io.on("connection", socket => {
+      var chatIO = this.io;
+      var messagingDB = this.messagingDB;
 
-        this.io.on('connection', (socket) => {
-            var chatIO = this.io
-            var messagingDB = this.messagingDB
+      this.loginSystem.retrieve_all_users().then(result => {
+        chatIO.to(socket.id).emit("initializeSearch", result);
+      });
 
-            this.loginSystem.retrieve_all_users().then( (result) => {
-              chatIO.to(socket.id).emit('initializeSearch', result)
-            });
-            
-            socket.on('initializeChat', async function (user) {
-              userIDs[user.userid] = socket.id;
-              socketIDs[socket.id] = user.userid;
-              socket.broadcast.emit('newConnectedUser', {userid: user.userid, socketid: socket.id});
-              var allUserChats = await messagingDB.retrieve_chat_groups(user.userid);
-              if (allUserChats.length > 0) {
-                var firstUserMessages = await messagingDB.retrieve_chat_messages(allUserChats[0].chat_id);
-                var currentUserIDs = [];
-                var chatIDs = [];
-                for (var i = 0; i < allUserChats.length; i++) {
-                  for (var j = 0; j < allUserChats[i].user_ids.length; j++) {
-                    if (allUserChats[i].user_ids[j] !== user.userid) {
-                      currentUserIDs.push(allUserChats[i].user_ids[j]);
-                    }
-                  }
-                  chatIDs.push(allUserChats[i].chat_id);
-                }
-                chatIO.to(userIDs[user.userid]).emit('retrieveFirstMessages', {IDs: currentUserIDs, messages: firstUserMessages, userChatIDs: chatIDs});
+      socket.on("initializeChat", async function(user) {
+        userIDs[user.userid] = socket.id;
+        socketIDs[socket.id] = user.userid;
+        socket.broadcast.emit("newConnectedUser", {
+          userid: user.userid,
+          socketid: socket.id
+        });
+        var allUserChats = await messagingDB.retrieve_chat_groups(user.userid);
+        if (allUserChats.length > 0) {
+          var firstUserMessages = await messagingDB.retrieve_chat_messages(
+            allUserChats[0].chat_id
+          );
+          var currentUserIDs = [];
+          var chatIDs = [];
+          for (var i = 0; i < allUserChats.length; i++) {
+            for (var j = 0; j < allUserChats[i].user_ids.length; j++) {
+              if (allUserChats[i].user_ids[j] !== user.userid) {
+                currentUserIDs.push(allUserChats[i].user_ids[j]);
               }
+            }
+            chatIDs.push(allUserChats[i].chat_id);
+          }
+          chatIO
+            .to(userIDs[user.userid])
+            .emit("retrieveFirstMessages", {
+              IDs: currentUserIDs,
+              messages: firstUserMessages,
+              userChatIDs: chatIDs
             });
+        }
+      });
 
-            //TODO: update friends list if user adds a new friend of the other person
-            //Current sample socket event before querying database
-            socket.on('newMessage', async function (msg) {
-              await messagingDB.send_message(msg.text, msg.userid, msg.chatid);
-              if (msg.userid2 in userIDs) {
-                chatIO.to(userIDs[msg.userid]).emit('receiveMessage', {text: msg.text, userid: msg.userid2});
-                chatIO.to(userIDs[msg.userid2]).emit('receiveMessage', {text: msg.text, userid: msg.userid});
-              } else {
-                chatIO.to(userIDs[msg.userid]).emit('receiveMessage', {text: msg.text, userid: msg.userid2});
-              }
-            });
+      //TODO: update friends list if user adds a new friend of the other person
+      //Current sample socket event before querying database
+      socket.on("newMessage", async function(msg) {
+        await messagingDB.send_message(msg.text, msg.userid, msg.chatid);
+        if (msg.userid2 in userIDs) {
+          chatIO
+            .to(userIDs[msg.userid])
+            .emit("receiveMessage", { text: msg.text, userid: msg.userid });
+          chatIO
+            .to(userIDs[msg.userid2])
+            .emit("receiveMessage", { text: msg.text, userid: msg.userid });
+        } else {
+          chatIO
+            .to(userIDs[msg.userid])
+            .emit("receiveMessage", { text: msg.text, userid: msg.userid });
+        }
+      });
 
-            socket.on('NewTwoPersonChat', async function (newChat) {
-              var newChatID = await messagingDB.create_two_user_chat_group(newChat.user1, newChat.user2);
-              //TODO: return value, doesExist for group chat later on
-              if (typeof newChatID !== "boolean") {
-                chatIO.to(userIDs[newChat.user1]).emit('AddedChat', {doesExist: false, userid: newChat.user2, chatid: newChatID, username: newChat.username});
-              } else {
-                chatIO.to(socket.id).emit("AddedChat", {doesExist: true});
-              }
+      socket.on("NewTwoPersonChat", async function(newChat) {
+        var newChatID = await messagingDB.create_two_user_chat_group(
+          newChat.user1,
+          newChat.user2
+        );
+        //TODO: return value, doesExist for group chat later on
+        if (typeof newChatID !== "boolean") {
+          chatIO
+            .to(userIDs[newChat.user1])
+            .emit("AddedChat", {
+              doesExist: false,
+              userid: newChat.user2,
+              chatid: newChatID,
+              username: newChat.username
             });
+          //chatIO.to(userIDs[newChat.user2]).emit('')
+        } else {
+          chatIO.to(socket.id).emit("AddedChat", { doesExist: true });
+        }
+      });
 
-            socket.on('changeChatUser', async function(user) {
-              var newMessages = await messagingDB.retrieve_chat_messages(user.chatID);
-              chatIO.to(socket.id).emit('retrieveNewChat', {chatid: user.chatID, messages: newMessages, username: user.username, userid: user.chatUserID});
-            });
-            
-            socket.on('addConnectedUser', (user) => {
-              userIDs[user.userid] = user.socketid;
-              socketIDs[user.socketid] = user.userid;
-            });
+      socket.on("changeChatUser", async function(user) {
+        var newMessages = await messagingDB.retrieve_chat_messages(user.chatID);
+        chatIO
+          .to(socket.id)
+          .emit("retrieveNewChat", {
+            chatid: user.chatID,
+            messages: newMessages,
+            username: user.username,
+            userid: user.chatUserID
+          });
+      });
 
-            socket.on('disconnect', () => {
-              var userid = socketIDs[socket.id];
-              delete userIDs[userid];
-              delete socketIDs[socket.id];
-            });
-          }); 
-    }
-} 
+      socket.on("addConnectedUser", user => {
+        userIDs[user.userid] = user.socketid;
+        socketIDs[user.socketid] = user.userid;
+      });
+
+      socket.on("disconnect", () => {
+        var userid = socketIDs[socket.id];
+        delete userIDs[userid];
+        delete socketIDs[socket.id];
+      });
+    });
+  }
+};
